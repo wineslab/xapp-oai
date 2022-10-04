03aff08bc814501820c522d5f7dbd53fe1a6de8c
Eugenio Moro <eugenio.moro@polimi.it>
2022-10-04 12:35:35 +0000

    first commit

---
+++ sample-xapp/run_xapp.py
@@ -0,0 +1,35 @@
+import logging
+from xapp_control import *
+
+
+def main():
+    # configure logger and console output
+    logging.basicConfig(level=logging.DEBUG, filename='/home/xapp-logger.log', filemode='a+',
+                        format='%(asctime)-15s %(levelname)-8s %(message)s')
+    formatter = logging.Formatter('%(asctime)-15s %(levelname)-8s %(message)s')
+    console = logging.StreamHandler()
+    console.setLevel(logging.INFO)
+    console.setFormatter(formatter)
+    logging.getLogger('').addHandler(console)
+    
+    control_sck = open_control_socket(4200)
+
+    while True:
+        logging.info("loop again")
+        data_sck = receive_from_socket(control_sck)
+        if len(data_sck) <= 0:
+            logging.info("leq 0 data")
+            if len(data_sck) == 0:
+                continue
+            else:
+                logging.info('Negative value for socket')
+                break
+        else:
+            logging.info('Received data: ' + repr(data_sck))
+            logging.info("Sending something back")
+            send_socket(control_sck, "test test test")
+
+
+if __name__ == '__main__':
+    main()
+
