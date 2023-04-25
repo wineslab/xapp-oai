97e57d67ce6a3123face14b5b1cd08e4c57056de
Eugenio Moro <eugenio.moro@polimi.it>
2023-04-25 15:58:14 +0000

    monitoring xapp

--- base-xapp/monitoring_xapp.py
+++ base-xapp/monitoring_xapp.py
@@ -8,18 +8,28 @@ from time import sleep
 
 def main():    
 
-    print("Encoding initial ric indication request")
+    print("Encoding ric monitoring request")
+    
+    # external message
     master_mess = RAN_message()
-    master_mess.msg_type = 1
-    print(RAN_message_type.SUBSCRIPTION)
     master_mess.msg_type = RAN_message_type.INDICATION_REQUEST
+
+    # internal message
     inner_mess = RAN_indication_request()
     inner_mess.target_params.extend([RAN_parameter.GNB_ID, RAN_parameter.UE_LIST])
-    #inner_mess.target_params.extend([RAN_parameter.GNB_ID])
+
+    # assign and serialize
     master_mess.ran_indication_request.CopyFrom(inner_mess)
     buf = master_mess.SerializeToString()
     xapp_control_ricbypass.send_to_socket(buf)
-    print("request sent, now waiting for incoming answers")
+    
+    while True:
+        r_buf = xapp_control_ricbypass.receive_from_socket()
+        ran_ind_resp = RAN_indication_response()
+        ran_ind_resp.ParseFromString(r_buf)
+        print(ran_ind_resp)
+        sleep(1)
+        xapp_control_ricbypass.send_to_socket(buf)
 
 
 if __name__ == '__main__':
